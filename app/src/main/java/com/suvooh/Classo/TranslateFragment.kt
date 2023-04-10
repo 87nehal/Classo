25704e51b2307ea56f243ae1138f887067186805
87nehal <87nehal@gmail.com>
2023-04-10 22:10:51 +0530

    Speak Added

--- app/src/main/java/com/suvooh/Classo/TranslateFragment.kt
+++ app/src/main/java/com/suvooh/Classo/TranslateFragment.kt
@@ -3,32 +3,55 @@ package com.suvooh.Classo
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
-import android.graphics.Rect
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.speech.tts.UtteranceProgressListener
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
 import com.suvooh.Classo.databinding.FragmentTranslateBinding
-import com.suvooh.Classo.Translate
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.util.*
 
-class TranslateFragment : Fragment() {
+class TranslateFragment : Fragment(),TextToSpeech.OnInitListener {
 
     private lateinit var binding: FragmentTranslateBinding
+    private lateinit var tts: TextToSpeech
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        tts = TextToSpeech(requireContext(), this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tts.stop()
+        tts.shutdown()
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = tts.setLanguage(Locale.JAPANESE)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported")
+            }
+        } else {
+            Log.e("TTS", "Initialization Failed!")
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentTranslateBinding.inflate(layoutInflater)
+        //textToSpeech = TextToSpeech(context, this)
 
         var srcLang="en"
         var targetLang="ja"
@@ -52,6 +75,7 @@ class TranslateFragment : Fragment() {
             binding.output.text = "No Internet"
             binding.input.isEnabled=false
             binding.langSelect.isEnabled=false
+            binding.speak.isEnabled=false
         }
 
         binding.input.addTextChangedListener {
@@ -69,8 +93,19 @@ class TranslateFragment : Fragment() {
             val clipData = ClipData.newPlainText("Output", binding.output.text.toString())
             clipboardManager.setPrimaryClip(clipData)
         }
+
+        binding.speak.setOnClickListener {
+            if (::tts.isInitialized) {
+                val text = binding.output.text.toString()
+                tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null)
+            } else {
+                Log.e("TTS", "TextToSpeech engine not initialized")
+            }
+        }
+        
         // Inflate the layout for this fragment
         return binding.root
     }
+
 }
 
